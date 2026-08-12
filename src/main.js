57256c7cf75eaef0fb53b5a9e72760f2e0ced938
useok <useok.jeju@gmail.cpm>
2026-08-12 22:31:30 +0900

    use beta theme for all showdown walls

--- src/main.js
+++ src/main.js
@@ -6937,8 +6937,8 @@ function resetGame() {
   stopAllBgm();
   crosshairEl.classList.remove("hidden");
   clock.getDelta();
-  // 베타 쇼다운은 시즌 번호와 관계없이 베타 쇼다운 테마를 사용한다.
-  const useIceCreamShowdown = IS_BETA_SEASON;
+  // 쇼다운은 시즌과 관계없이 베타의 얼음 벽·바닥 테마를 사용한다.
+  const useIceCreamShowdown = true;
   battleMapGroup.visible = !useIceCreamShowdown;
   trainingMapGroup.visible = false;
   showdownMapGroup.visible = useIceCreamShowdown;
