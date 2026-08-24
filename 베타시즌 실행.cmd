2269e972812a7a8e29f91238cdf9a6f954b6491f
useok <useok.jeju@gmail.cpm>
2026-08-24 20:48:11 +0900

    fix: open beta season 5 test from launcher

--- 베타시즌 실행.cmd
+++ 베타시즌 실행.cmd
@@ -7,6 +7,6 @@ powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command ^
   "$server = Get-NetTCPConnection -LocalPort 4173 -State Listen -ErrorAction SilentlyContinue;" ^
   "if (-not $server) { Start-Process py -ArgumentList '-m','http.server','4173' -WorkingDirectory $root -WindowStyle Hidden };" ^
   "Start-Sleep -Milliseconds 800;" ^
-  "Start-Process 'http://localhost:4173/beta-season.html'"
+  "Start-Process 'http://localhost:4173/beta-season.html?test=beta5'"
 
 endlocal
